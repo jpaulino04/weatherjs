be68a4951981d42db9c0f07b3d53c0d6f9fb215a
Jose Paulino <jpaulino04@msn.com>
2018-08-15 11:24:08 -0400

    Small Weather API Completed

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
+//Init Storage
+const storage = new Storage();
+//Get stored location data
+const weatherLocation = storage.getLocationData();
 // Init weather object
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+//Init UI object
+const ui = new UI;
 
-weather.changeLocation('Queens', 'NY')
+//Get Weather on DOM Load
+document.addEventListener('DOMContentLoaded', getWeather);
 
-weather.getWeather()
+// Change Location Event
+document.getElementById('w-change-btn').addEventListener('click', e => {
+
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+  
+
+  //Change Location
+  weather.changeLocation(city, state)
+
+  //Set location in Loc.Sto
+  storage.setLocationData(city, state);
+
+  //Get and display weather
+  getWeather();
+
+  //Close modal
+  $('#locModal').modal('hide');
+})
+
+
+
+
+//get weather function
+function getWeather(){
+  weather.getWeather()
   .then(results => {
-    console.log(results)
+    ui.paint(results);
   })
   .catch(err => console.log(err))
+}
 
